1bb9e09a4573a85f0fa5162ac308778ba0db98a1
Ardalan Samimi <ardalan.samimi@gmail.com>
2015-10-12 13:08:32 +0200

    2.5.3 (06B4)
    Changes:
    * Fixed support for copy, paste and cut command shortcuts while editing
    textfields in the exclude list table.

--- ComicZipper/CZApplication.m
+++ ComicZipper/CZApplication.m
@@ -15,9 +15,27 @@
     if ([theEvent type] == NSKeyUp) {
         [[[self mainWindow] firstResponder] tryToPerform:@selector(keyUp:) with:theEvent];
         return;
+    } else if ([theEvent type] == NSKeyDown) {
+        // Quick fix for fixing keyboard shortcut in textfields from http://stackoverflow.com/questions/970707/cocoa-keyboard-shortcuts-in-dialog-without-an-edit-menu
+        if (([theEvent modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([[theEvent charactersIgnoringModifiers] isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            } else if ([[theEvent charactersIgnoringModifiers] isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            } else if ([[theEvent charactersIgnoringModifiers] isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            } else if ([[theEvent charactersIgnoringModifiers] isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
     }
     
     [super sendEvent:theEvent];
 }
+
 
 @end
